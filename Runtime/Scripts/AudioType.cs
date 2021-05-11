8e116d60a068b31d90ba59d9c12523cb689f4dd1
AndyKennedy-Github <61878684+AndyKennedy-Github@users.noreply.github.com>
2021-05-11 15:20:04 -0400

    Work From Tuesday
    - Wrote out AudioType class
    -Wrote most of AudioController (need to finish coroutine)

---
+++ Runtime/Scripts/AudioType.cs
@@ -0,0 +1,19 @@
+
+namespace aek.Audio
+{
+    namespace UnityCore
+    {
+        namespace Audio
+        {
+            public enum AudioType
+            {
+                None,
+                ST_01,
+                ST_02,
+                SFX_01,
+                SFX_02,
+                //.. put custom audio types here
+            }
+        }
+    }
+}
---
+++ Runtime/Scripts/TestAudio.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace aek.Audio
+{
+    namespace UnityCore
+    {
+        namespace Audio
+        {
+            public class TestAudio : MonoBehaviour
+            {
+                public AudioController ac;
+
+                #region Unity Functions
+#if UNITY_EDITOR
+                private void Update()
+                {
+                    if(Input.GetKeyUp(KeyCode.T))
+                    {
+                        ac.PlayAudio(AudioType.ST_01);
+                    }
+                    if (Input.GetKeyUp(KeyCode.G))
+                    {
+                        ac.StopAudio(AudioType.ST_01);
+                    }
+                    if (Input.GetKeyUp(KeyCode.B))
+                    {
+                        ac.RestartAudio(AudioType.ST_01);
+                    }
+
+                    if (Input.GetKeyUp(KeyCode.Y))
+                    {
+                        ac.PlayAudio(AudioType.SFX_01);
+                    }
+                    if (Input.GetKeyUp(KeyCode.H))
+                    {
+                        ac.StopAudio(AudioType.SFX_01);
+                    }
+                    if (Input.GetKeyUp(KeyCode.N))
+                    {
+                        ac.RestartAudio(AudioType.SFX_01);
+                    }
+                }
+#endif
+                #endregion
+            }
+        }
+    }
+}
